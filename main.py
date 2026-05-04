97389623fab5044b9364f7d028867032d024e6b8
Angelsit0 <angelgabrieltp2006@gmail.com>
2026-05-04 17:11:34 -0400

    feat:  importacion de .json -valido usuario y documento -se hace un limite de intentos fallidos -se agrega ciclo while para poder lograr un limite de intentos importando la informacion del .json y a su vez while(true) para crear un menu interactivo con el usuario - se imprimen distintas opciones - opcion con funcionalidad ( 2) agregar gasto con un range() y hacer el calculo de los gatos con un acumulador

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-user = "Angel"
-clave = 32984739
+import json 
 totalgastos = 0
 
-ingreso = input("Ingrese su usuario: ")
-documento=int(input("Ingrese su documento: "))
-if(user == ingreso ) and (clave == documento):
-    print("Usted ha ingresado con exito")
-    print("Bienvenido Al Sistema De Control De Gastos (Angel Opera)")
-    print("USUARIO: " + user  )
-    print("Su documento valido es:", documento)
-    print("Cuanto dinero tiene disponible al dia de hoy ")
-    dinero=float(input())
+with open ("config.json","r") as file:
+    config = json.load(file)
 
-    numero_gastos = int(input("Cuantos gastos desea ingresar: "))
-    for i in range(numero_gastos):
+intentos = 0
+while(intentos < config["intentos"]):
+    user = input("Indique su  usuario: \n")
+    password = input("Indique su  documento valido: \n")  
+    if(user == config["usuario"]) and (password == config["clave"]):
+        print("-------------- Ha ingresado al sistema de Control De Gastos--------------")
+        print("Bienvenido " + user + " 🥰 ")
 
-        gastos_hechos = float(input("Ingrese el gasto " + str(i+1) + ":"))
-        totalgastos= totalgastos + gastos_hechos
+        while(True):
+            print("-------------- OPCIONES A ELEGIR-------------- ")
+            print("1) Agregar dinero")
+            print("2) Agregar Gasto ")
+            print("3) Ver historial")
+            print("4) Salir")
+            opcion=int(input("Seleccione alguna de las opciones: \n"))
+            if(opcion == 2 ):
+                    numero_gastos = int(input("Cuantos gastos desea ingresar: \n"))
+                    for i in range(numero_gastos):
+                        gastos_hechos = float(input("Ingrese el gasto " + str(i+1) + ":\n"))
+                        totalgastos= totalgastos + gastos_hechos
+                    print("Usted gasto hoy un total de ",round(totalgastos,2))
+
+    else:
+        print("Datos ingresados erroneos")
+        intentos = intentos + 1
+        
 
 
-    print("-----------------------PROCESANDO GASTO-------------------------")
-    print(".....")
-    print(".....")
-    print(".....")
-    operacion=(dinero-totalgastos)
-    print("Usted gasto hoy un total de ",round(totalgastos,2))
-    print("En su cuenta queda un total de ",round(operacion,2))
-    if(totalgastos>dinero):
-        print(user +", Te has excedido con los gastos hoy ")
-    if(operacion<0):
-        print(user+ ", Tines una deuda de: ", operacion )
-else:
-    print("Usted ha introducido algun dato erroneo")
