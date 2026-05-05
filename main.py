adb2c4293b323fde3528a53b79da556d794b2bfc
Angelsit0 <angelgabrieltp2006@gmail.com>
2026-05-05 00:03:59 -0400

    feat: funcionalidades nuevas en el menu - lee el saldo disponible del .json y lo muestra para despues sumarle el dinero que se le agregue -agregar gastos - imprimir balance

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json 
 totalgastos = 0
+intentos = 0
 
 with open ("config.json","r") as file:
     config = json.load(file)
 
-intentos = 0
+
 while(intentos < config["intentos"]):
     user = input("Indique su  usuario: \n")
     password = input("Indique su  documento valido: \n")  
@@ -16,15 +17,28 @@ while(intentos < config["intentos"]):
             print("-------------- OPCIONES A ELEGIR-------------- ")
             print("1) Agregar dinero")
             print("2) Agregar Gasto ")
-            print("3) Ver historial")
-            print("4) Salir")
+            print("3) Ver historial de movimientos")
+            print("4) Ver Balance")
+            print("5) Salir")
             opcion=int(input("Seleccione alguna de las opciones: \n"))
-            if(opcion == 2 ):
+
+
+            if(opcion == 1): 
+                 print("Su saldo disponible es: " + config["saldo"])
+                 print("Cuanto dinero desea transferir a su cuenta el dia de hoy: \n")
+                 dinero_agregado=float(input())
+                 config["saldo"]= config["saldo"]  + dinero_agregado
+
+            
+            elif(opcion == 2 ):
                     numero_gastos = int(input("Cuantos gastos desea ingresar: \n"))
                     for i in range(numero_gastos):
                         gastos_hechos = float(input("Ingrese el gasto " + str(i+1) + ":\n"))
                         totalgastos= totalgastos + gastos_hechos
                     print("Usted gasto hoy un total de ",round(totalgastos,2))
+            
+            elif(opcion == 4):
+                 print("Su balance disponible es: ", config["saldo"])
 
     else:
         print("Datos ingresados erroneos")
